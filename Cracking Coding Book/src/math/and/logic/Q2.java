a630aefe87137ff12a0f547aa1382c6bb81c4655
anasmekky <anas.mekky@foodycraft.com>
2025-01-08 09:27:34 +0000

    math and logic

---
+++ Cracking Coding Book/src/math/and/logic/Q2.java
@@ -0,0 +1,37 @@
+package math.and.logic;
+/*
+Question:
+You have a basketball hoop and someone says that you can play one of two games:
+- Game 1: You get one shot to make the hoop.
+- Game 2: You get three shots and you have to make at least two out of three shots.
+If p is the probability of making a particular shot, for which values of p should you pick one game or the other?
+*/
+
+public class Q2 {
+
+    public static void main(String[] args) {
+        double p = 0.4;
+
+        if (p < 0 || p > 1) {
+            System.out.println("Invalid value for p. It should be between 0 and 1.");
+            return;
+        }
+
+        double game1Probability = p;
+        double game2Probability = calculateGame2Probability(p);
+
+        if (game1Probability > game2Probability) {
+            System.out.println("You should choose Game 1.");
+        } else if (game2Probability > game1Probability) {
+            System.out.println("You should choose Game 2.");
+        } else {
+            System.out.println("Both games have equal probabilities. It doesn't matter which one you choose.");
+        }
+    }
+
+    public static double calculateGame2Probability(double p) {
+        double allThree = Math.pow(p, 3);
+        double exactlyTwo = 3 * Math.pow(p, 2) * (1 - p);
+        return allThree + exactlyTwo;
+    }
+}
---
+++ Cracking Coding Book/src/math/and/logic/Q3.java
@@ -0,0 +1,41 @@
+package math.and.logic;
+/**
+ * There is an 8×8 chessboard in which two diagonally opposite corners have been removed.
+ * You are given 31 dominoes, and each domino can cover exactly two squares.
+ * Can you use the 31 dominoes to cover the entire board? Prove why it is impossible.
+ */
+public class Q3 {
+
+    public static void main(String[] args) {
+        int boardSize = 8;
+        int[][] board = new int[boardSize][boardSize];
+
+        board[0][0] = -1;
+        board[7][7] = -1;
+
+        int whiteSquares = 0;
+        int blackSquares = 0;
+
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (board[i][j] == -1) {
+                    continue;
+                }
+                if ((i + j) % 2 == 0) {
+                    blackSquares++;
+                } else {
+                    whiteSquares++;
+                }
+            }
+        }
+
+        System.out.println("Number of white squares: " + whiteSquares);
+        System.out.println("Number of black squares: " + blackSquares);
+
+        if (blackSquares != whiteSquares) {
+            System.out.println("It is impossible to cover the board with 31 dominoes.");
+        } else {
+            System.out.println("It is theoretically possible to cover the board with 31 dominoes (but constraints make it impossible).");
+        }
+    }
+}
